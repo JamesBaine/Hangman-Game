7a0353f6e869d57917136042593d9bed6752c99d
James Baine <jamesbaine@Jamess-MacBook-Pro.local>
2017-05-14 14:34:58 -0700

    JS game mechanics basically done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,140 @@
+var start = false;
+
 var wins = 0;
 
-var word = [];
+var losses = 0;
 
-var left = 7;
+var guessesLeft = 7;
 
-var guessed = [];
+var wordsList = ['anthrax','megadeth','slayer','metallica', 'testament'];
+
+var answer;
+
+var guesses = [];
+
+var blanksAndSuccesses = [];
+
+var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+// startGame()
+// Its how we we will start and restart the game.
+// (Note: It's not being run here. It's just being made for future use.)
+function startGame() {
+
+	document.getElementById("start").style.display = "none";
+
+	guessesLeft = 7;
+
+	blanksAndSuccesses = [];
+
+	var randomWord = wordsList[Math.floor(Math.random()*wordsList.length)];
+
+	answer = randomWord.split("");
+
+	var numBlanks = answer.length;
+
+	
+	for ( var i = 0; i < numBlanks; i++){
+		
+		 blanksAndSuccesses.push("_");
+
+	}
+
+	
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("guessesLeft").innerHTML = guessesLeft;
+	document.getElementById("guesses").innerHTML = guesses;
+	document.getElementById("wins").innerHTML = wins;
+	document.getElementById("losses").innerHTML = losses;
+
+	console.log(answer);
+  }
 
 
 
-document.onkeyup = function(event) {
-	document.getElementById("start").style.display = 'none';
+ function checkLetters(letter) {
+
+
+ 	if ( answer.indexOf(letter) !== -1) {
+
+ 		for ( var i = 0; i < answer.length; i++ ) {
+	 			if ( answer[i] === letter){
+	 				blanksAndSuccesses[i] = letter;
+	 			} 
+			}
+
+		}
+
+	if (answer.indexOf(letter) === -1) {
+			guesses.push(letter);
+			guessesLeft--;
+
+		}
+
+
+
+
+
+		
+
+
+
+
+
+
+
 
 }
+
+ 	
+
+ function roundComplete() {
+ 	
+
+ 	document.getElementById("guessesLeft").innerHTML = guessesLeft;
+ 	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+ 	document.getElementById("guesses").innerHTML = guesses;
+
+ 	if ( blanksAndSuccesses.indexOf("_") === -1 ) {
+ 		wins++;
+ 		alert("You won!");
+ 		document.getElementById("wins").innerHTML = wins;
+ 		startGame();
+ 	}
+
+ 	if ( guessesLeft === 0 ) {
+ 		losses++;
+ 		alert("You lose!");
+ 		document.getElementById("losses").innerHTML = losses;
+ 		startGame();
+ 	}
+
+
+
+ }
+
+startGame();
+
+document.onkeyup = function(event) {
+
+
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+
+	if ( alphabet.indexOf(letterGuessed) === -1 
+		|| guesses.indexOf(letterGuessed) !== -1 
+		|| blanksAndSuccesses.indexOf(letterGuessed) !== -1 ) {
+		return;
+	}
+		
+	checkLetters(letterGuessed);
+
+	roundComplete();
+	
+	
+
+}
+	
+
+
+
